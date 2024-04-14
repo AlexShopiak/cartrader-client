1434c71112957168a7d2f2290c40f2d14979ae03
Alex Shopiak <alshop2004@gmail.com>
2024-04-14 23:09:55 +0000

    con

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,11 @@ export default function Home() {
   const [offerListings, setOfferListings] = useState([]);
   const [saleListings, setSaleListings] = useState([]);
   const [rentListings, setRentListings] = useState([]);
+
+  ////
+  const storedConsent = localStorage.getItem('cookieConsent');
+  alert("CONSENT", storedConsent);
+  ////
   SwiperCore.use([Navigation]);
   console.log(offerListings);
   useEffect(() => {
