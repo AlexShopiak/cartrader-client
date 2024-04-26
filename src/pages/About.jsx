6d1b33c48931ef5d43dd5b012eae651761b5d18a
Alex Shopiak <alshop2004@gmail.com>
2024-04-26 16:37:49 +0000

    Add credits

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,6 +7,8 @@ export default function About() {
       <p className='mb-4 text-slate-700'>Car Trader is a leading car trader website that specializes in helping people buy or sell user or new cars. Our team is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.</p>
       <p className='mb-4 text-slate-700'>Our mission is to help our clients achieve their goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy or sell a vehicle, we are here to help you every step of the way.</p>
       <p className='mb-4 text-slate-700'>Our team has a wealth of experience and knowledge in the car industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a car should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+      <p className='mb-4 text-slate-700'>Made by ALEX SHOPIAK.</p>
+      <a href="https://github.com/AlexShopiak/cartrader" className="text-blue-500 underline">GitHub</a>
     </div>
   )
 }
